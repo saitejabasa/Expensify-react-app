ceb707866f73179ed56a1a6f93aa0aaca49631e3
Saiteja Basa <sunnysai808@gmail.com>
2019-12-03 23:01:26 +0530

    code changes

--- src/app.js
+++ src/app.js
@@ -12,15 +12,7 @@ import './styles/styles.scss';
 const store = configureStore(); 
 
 
-store.dispatch(addExpense({ description: 'Gas bill' , amount:'50'}));
-store.dispatch(addExpense({ description: 'Water bill' , createdAt:'2100'}));
-store.dispatch(addExpense({ description: 'Rent' , amount:'308100'}));
-store.dispatch(addExpense({ description: 'House' , amount:'20000',createdAt:'100'}));
-
-
 const state = store.getState();
-// const visibleExpenses = getVisibleExpenses(state.expenses, state.filters);
-// console.log("visibleExpenses::", visibleExpenses);
 
 const jsx = (
 
