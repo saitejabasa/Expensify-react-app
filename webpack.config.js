ebc7e8d2a27836191cc97cc1f1e7bd216f29112d
Saiteja Basa <sunnysai808@gmail.com>
2019-12-06 00:42:34 +0530

    modified files

--- webpack.config.js
+++ webpack.config.js
@@ -9,7 +9,7 @@ module.exports = (env) => {
     entry: './src/app.js',
     mode: isProduction ? isProduction : 'development',
     output: {
-      path: path.join(__dirname, 'public'),    
+      path: path.join(__dirname, 'public', 'dist'),    
       filename: 'bundle.js'
     },
     module: {
@@ -55,7 +55,8 @@ module.exports = (env) => {
     devtool: isProduction? "source-map":'inline-source-map',
     devServer: {
       contentBase: path.join(__dirname, 'public'),
-      historyApiFallback:true
+      historyApiFallback:true,
+      publicPath:'/dist/'
     }
   };
 };
