453b766fd01ea655f240f229b13227edf65a4221
Saiteja Basa <sunnysai808@gmail.com>
2019-12-06 10:49:48 +0530

    small changes in rs

--- src/components/ExpenseListItems.js
+++ src/components/ExpenseListItems.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
+import moment from 'moment';
+import numeral from 'numeral';
 // import { removeExpense } from '../actions/expenses';
 
 const ExpenseListItems = ({ description, amount, createdAt , id})=>(
@@ -8,8 +10,10 @@ const ExpenseListItems = ({ description, amount, createdAt , id})=>(
         <Link to = {`/edit/${id}`}>
             <h2>{description}</h2>
         </Link>
-        <p>{amount} - {createdAt}</p>
-        
+        <p>
+            {numeral(amount/100).format('$0,0.00')} 
+            - 
+            {moment(createdAt).format('MMMM-Do-YYYY')}</p> 
     </div>
 );
 
